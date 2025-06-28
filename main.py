595471e5aae0f9716a06377481c8bfab22479778
004Saichaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2025-06-28 21:55:19 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def calculate_sum(a, b):
-    """Returns the sum of two numbers."""
-    return a + b
-
-
-def multiply_numbers(x, y):
-    """Returns the product of two numbers."""
-    return x * y
